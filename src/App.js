dcc11e8725cac927385d895d527ad127dc282728
Rafa <rafa@MacBook-Pro-de-Rafa.local>
2021-08-05 07:38:27 +0200

    feat: add tasks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
-import 'semantic-ui-css/semantic.min.css'
-import './App.css';
-import Container from './components/Container';
-import Header from './components/Header';
-import InputTask from './components/InputTask/InputTask';
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+import { useState, useEffect } from "react";
+
+import Container from "./components/Container";
+import Header from "./components/Header";
+import InputTask from "./components/InputTask/InputTask";
+import TasksContent from "./components/TasksContent";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  // TODO: Pasar las citas a localstorage
+  let initialTasks = JSON.parse(localStorage.getItem("tasks"));
+  console.log(initialTasks);
+  if (!initialTasks) {
+    initialTasks = [];
+  }
+
+  useEffect(() => {
+    if (initialTasks) {
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    } else {
+      localStorage.setItem("tasks", JSON.stringify([]));
+    }
+    console.log(initialTasks);
+  }, [tasks]);
+  // END TODO
+
+  // Crear las tareas
+  const createTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
+  // función que elimina una cita por su id
+  const deleteTask = (id) => {
+    const currentTasks = tasks.filter((task) => task.idTask !== id);
+    setTasks(currentTasks);
+  };
+
   return (
     <>
       <Container>
         <Header />
-        <InputTask />
+        <InputTask createTask={createTask} />
+        <TasksContent tasks={tasks} deleteTask={deleteTask} />
       </Container>
     </>
   );
---
+++ src/components/Task/Task.js
@@ -0,0 +1,25 @@
+import { Grid, Segment, Label, Header, Button, Icon } from "semantic-ui-react";
+
+export default function Task(props) {
+  const { task, deleteTask } = props;
+  const { idTask, taskName, categoryTask } = task;
+
+  return (
+    <Grid.Column width={8} className="task-container">
+      <Segment>
+        {categoryTask && (
+          <Label color="teal" ribbon="right">
+            {categoryTask}
+          </Label>
+        )}
+        <Header as="h3" className="header-task">
+          {taskName}
+        </Header>
+        <Header as="h5">{idTask}</Header>
+        <Button color="red" onClick={() => deleteTask(idTask)}>
+          <Icon name="remove circle" /> Eliminar
+        </Button>
+      </Segment>
+    </Grid.Column>
+  );
+}
